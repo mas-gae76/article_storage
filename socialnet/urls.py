9f202a57b513d91a0b648e58216b83529113d243
mas-gae76 <alex2904.goryachev@yandex.com>
2022-05-22 01:46:38 +0300

    Configured routing

--- socialnet/urls.py
+++ socialnet/urls.py
@@ -5,4 +5,10 @@ from .views import *
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('signup/', RegisterView.as_view(), name='signup'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
+    path('', ArticleView.as_view(), name='index'),
+    path('favourites/', FavouriteView.as_view(), name='favourites'),
+    path('preferences/', SettingsView.as_view(), name='preferences'),
+    path('add/', CreateArticleView.as_view(), name='add'),
+    path('<slug:slug>/', DetailArticle.as_view(), name='detail'),
 ]
